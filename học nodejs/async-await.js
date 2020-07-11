6fa0fc303b220764dc5063f284ed5a80a6e2b52b
tran nhat <tranminhnhat1005@gmail.com>
2020-07-11 23:51:15 +0700

    Create async-await.js

---
+++ học nodejs/async-await.js
@@ -0,0 +1,11 @@
+let mang = [1, 4, 4, 5, 2, 4];
+
+// mang.forEach (e => console.log (e));
+
+var mang2 = mang.map (e => e * 2);
+
+// console.log(mang2);
+
+let add = (a, b) => a + b;
+
+console.log (add (89, 900));
